8acb72c6419886dacac3205fd797f1b5e6c0d69d
Rohan Salunkhe <rohan.salunkhe@digital.justice.gov.uk>
2021-11-18 15:07:12 +0000

    pagination links

--- WEB-INF/src/com/MOJICT/UBI/Actions/csvdownloadAction.java
+++ WEB-INF/src/com/MOJICT/UBI/Actions/csvdownloadAction.java
@@ -4,16 +4,20 @@
 
 package com.MOJICT.UBI.Actions;
 
+
 import com.MOJICT.UBI.Util.DBConnection;
 import com.MOJICT.UBI.Util.Validator;
 
 import javax.servlet.ServletException;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import org.hibernate.Query;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Session;
@@ -25,8 +29,10 @@ import com.library.plugin.HibernatePlugin;
 
 import org.hibernate.SessionFactory;
 
+import com.MOJICT.UBI.Beans.Data;
 import com.MOJICT.UBI.Forms.DataForm;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForward;
 
@@ -37,104 +43,75 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.Action;
 
-public class SearchAction extends Action
+public class csvdownloadAction extends Action
 {
     String strQry;
     Boolean flag;
     Boolean dateFlag;
 
-    public SearchAction() {
+    public csvdownloadAction() {
         this.flag = false;
         this.dateFlag = false;
     }
 
     public ActionForward execute(final ActionMapping mapping, final ActionForm form, final HttpServletRequest request, final HttpServletResponse response) throws IOException, ServletException, Exception {
-    	Logger logger = Logger.getLogger(SearchAction.class);
+    	Logger logger = Logger.getLogger(csvdownloadAction.class);
     	Session session = null;
         SessionFactory factory = null;
-        List arrResults = null;
+        List<String> arrResults = null;
         Query qry = null;
-        this.strQry = "from Data data where ";
+        this.strQry = "from Data";
         this.dateFlag = false;
-        
         try {
-            DataForm frm = (DataForm)form;
-            //factory = (SessionFactory)this.servlet.getServletContext().getAttribute(HibernatePlugin.KEY_NAME);
-            factory = DBConnection.getDBConnection();
+        	factory = DBConnection.getDBConnection();
             session = (Session)factory.openSession();
-            String name=frm.getName().trim().toLowerCase();
-            String case_number=frm.getCase_number().trim();
-
-            String msg = "";
-            if (frm.getName().trim() == null || frm.getName().trim().equals("")) {
-                msg = "required";
-                
-            }
-            else {
-                msg.equals(this.validateform(frm));
-            }
-            String genericerror="";
-            if (msg != "") {
-                if (msg.equals("date")) {
-                    genericerror=genericerror+"dateerror";
-                    request.setAttribute("genericerror", genericerror);
-                	request.setAttribute("dateError", (Object)"date");
-                }
-                else if (msg.equals("year")) {
-                	genericerror=genericerror+"yearerror";
-                	request.setAttribute("genericerror", genericerror);
-                    request.setAttribute("yearError", (Object)"year");
-                }
-                else if (msg.equals("invalid")) {
-                	genericerror=genericerror+"invalidError";
-                	request.setAttribute("genericerror", genericerror);
-                    request.setAttribute("invalidError", (Object)"invalid");
-                }
-                else if (msg.equals("required")) {
-                	genericerror=genericerror+"requirederror";
-                	request.setAttribute("genericerror", genericerror);
-                    request.setAttribute("requiredError", (Object)"required");
-                }
-                return mapping.findForward("failure");
-            }
-            if (!this.flag) {
-            	genericerror=genericerror+"fielderror";
-            	request.setAttribute("genericerror", genericerror);
-                request.setAttribute("fieldError", (Object)"fieldError");
-                return mapping.findForward("failure");
-            }
-            qry = session.createQuery(this.strQry);
-            qry.setString("searchname", "%"+name+"%");
-            if (frm.getCase_number().trim() != null && !frm.getCase_number().equals("")) {
-                if (Validator.IsValidNumber(frm.getCase_number())) {
-            qry.setString("case_number", "%"+case_number+"%");
-                }
-                }
             
-            logger.info(this.strQry);
-            if (this.dateFlag) {
-                final int frm_year = Integer.parseInt(frm.getFrom_year()) + 2000;
-                final String from_year = String.valueOf(frm_year);
-                final int too_year = Integer.parseInt(frm.getTo_year()) + 2000;
-                final String to_year = String.valueOf(too_year);
-                qry.setDate("from_date", (Date)myDate.String2Date(String.valueOf(frm.getFrom_day()) + "/" + frm.getFrom_month() + "/" + from_year));
-                qry.setDate("to_date", (Date)myDate.String2Date(String.valueOf(frm.getTo_day()) + "/" + frm.getTo_month() + "/" + to_year));
-            }
-            arrResults = qry.list();
+            qry = session.createQuery(this.strQry);
+            //logger.info(this.strQry);
            
-            frm = null;
-            //qry.
-            int noOfPages=arrResults.size()/50;
-            int page=1;
-            request.setAttribute("results", (List)arrResults);
-            request.setAttribute("noOfPages", noOfPages);
-            request.setAttribute("currentPage", page);
+            arrResults = qry.list();
+            
+            String commalist="";
+            Data data=null;
+            //FileWriter writer = new FileWriter("/files/data.csv");
+            for(Object map : arrResults)
+            {
+            	data = (Data) map;
+            	commalist=commalist+data.getPrime_index()+","+data.getCase_number()+","+data.getYear_carried()+","+data.getCredit_detail()+","+data.getDate_account()+"\n";
+            	
+            	
+            	//System.out.println(data.getPrime_index());
+            	//System.out.println(data.getCase_number());
+            	
+            	
+            }
 
+          // commalist = StringUtils.join(arrResults.toArray(), ",");
+            //System.out.println(commalist);
+
+           // writer.write(commalist);
+           // writer.close();
+            response.setContentType("text/csv");
+            response.setHeader("Content-Disposition", "attachment; filename=\"userDirectory.csv\"");
+            try
+            {
+                OutputStream outputStream = response.getOutputStream();
+                //String outputResult = "xxxx, yyyy, zzzz, aaaa, bbbb, ccccc, dddd, eeee, ffff, gggg\n";
+                outputStream.write(commalist.getBytes());
+                System.out.println(commalist.toString());
+                outputStream.flush();
+                outputStream.close();
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.toString());
+            }
+            
         }
         catch (Exception ex) {
 
-            request.setAttribute("results",  (List)arrResults);
-           logger.info("Its coming here in catch exception");
+            //request.setAttribute("results", (Object)arrResults);
+           // System.out.println("Its coming here in catch exception");
             ex.printStackTrace();
         }
         finally
@@ -145,15 +122,13 @@ public class SearchAction extends Action
         }
         return mapping.findForward("success");
     }
+
     private String validateform(final DataForm frm) {
         String errMsg = "";
         this.flag = false;
-        String name=frm.getName().trim().toLowerCase();
-        String case_number=frm.getCase_number().trim();
         if (frm.getName().trim() != null && !frm.getName().trim().equals("")) {
             if (Validator.IsValidName(frm.getName())) {
-                //this.strQry = String.valueOf(this.strQry) + " (lower(data.prime_index) like '%" +  + "%' OR lower(data.credit_detail) like '%" + frm.getName().trim().toLowerCase() + "%' )";
-            	this.strQry = String.valueOf(this.strQry) + " (lower(data.prime_index) like :searchname OR lower(data.credit_detail) like :searchname)";
+                this.strQry = String.valueOf(this.strQry) + " (lower(data.prime_index) like '%" + frm.getName().trim().toLowerCase() + "%' OR lower(data.credit_detail) like '%" + frm.getName().trim().toLowerCase() + "%' )";
                 this.flag = true;
             }
             else {
@@ -163,10 +138,10 @@ public class SearchAction extends Action
         if (frm.getCase_number().trim() != null && !frm.getCase_number().equals("")) {
             if (Validator.IsValidNumber(frm.getCase_number())) {
                 if (this.flag) {
-                    this.strQry = String.valueOf(this.strQry) + "and data.case_number like :case_number ";
+                    this.strQry = String.valueOf(this.strQry) + "and data.case_number like '%" + frm.getCase_number().trim() + "%' ";
                 }
                 else {
-                    this.strQry = String.valueOf(this.strQry) + "data.case_number like :case_number";
+                    this.strQry = String.valueOf(this.strQry) + "data.case_number like '" + frm.getCase_number().trim() + "%' ";
                 }
                 this.flag = true;
             }
--- tiles/results.jsp
+++ tiles/results.jsp
@@ -2,9 +2,13 @@
 <%@ taglib uri="/WEB-INF/tld/struts-bean.tld" prefix="bean" %>
 <%@ taglib uri="/WEB-INF/struts-logic.tld" prefix="logic" %>
 <%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html" %>
+<%@ taglib uri="/WEB-INF/c.tld" prefix="c"%>
+
+<%@ page import="java.util.List" %>
 <%@ page buffer = "32kb" %>
 
 			<a href="javascript:history.go(-1)" class="govuk-back-link">Back</a>							
+
 
 			<h1 class="govuk-heading-xl">Search results</h1>
 
@@ -14,18 +18,40 @@
 				</logic:present>
 
 				<section id="js-results-table"><!-- required for styling using JavaScript (see ubi-specific.js) -->
-        <logic:present name="results">
-
+       			 <logic:present name="results">
+	                     <%  List results= request.getParameter("results");
+								int total=50;  
+	                            int currentPage=0;
+								
+								String spageid=request.getParameter("page"); 
+								//int noOfPages=results.size()/50;
+								System.out.println("no of pages"+results.size()/50);
+								request.set
+								int pageid=Integer.parseInt(spageid);  
+								
+								if(pageid==1){
+								currentPage=1;
+								}  
+								else{  
+								    currentPage=pageid;
+								    pageid=pageid-1;  
+								    pageid=pageid*total+1;  
+								}  
+								List list=results.(pageid,total*pageid);
+								  
+								  
+								out.print("<h1>Page No: "+spageid+"</h1>");  
+						%>  
 					<p class="govuk-body"><a class="govuk-button" href="downloadResults.do?name=<%=request.getParameter("name") %>&case_number=<%=request.getParameter("case_number")%>&from_day=<%=request.getParameter("from_day")%>&from_month=<%=request.getParameter("from_month")%>&from_year=<%=request.getParameter("from_year")%>&to_day=<%=request.getParameter("to_day")%>&to_month=<%=request.getParameter("to_month")%>&to_year=<%=request.getParameter("to_year")%>">Download results as CSV</a></p>
-					<tbody>
-						<logic:present name="result">
+					<table>
+						
 						<tr>
 						     <td>Account number</td>
 						     <td>Case name</td>
 						     <td>Date account opened</td>
 						    
 						 </tr>
-						<logic:iterate id="bean" name="result">
+						<logic:iterate id="bean" name="results">
 						   <tr>
 						     <td><bean:write name="bean" property="case_number"/></td>
 						     <td><bean:write name="bean" property="prime_index"/></td>
@@ -33,9 +59,34 @@
 						    
 						   </tr>
 						</logic:iterate>
-						</logic:present>
-					</tbody>
-											
+							/*  	<c:if test="${currentPage != 1}">
+						        <td><a href="searchResults.do?page=${currentPage - 1}">Previous</a></td>
+						    </c:if>
+						 
+						  <%--For displaying Page numbers. 
+						    The when condition does not display a link for the current page--%>
+						    <table border="1" cellpadding="5" cellspacing="5">
+						        <tr>
+						            <c:forEach begin="1" end="${noOfPages}" var="i">
+						                <c:choose>
+						                    <c:when test="${currentPage eq i}">
+						                        <td>${i}</td>
+						                    </c:when>
+						                    <c:otherwise>
+						                        <td><a href="searchResults.do?page=${i}">${i}</a></td>
+						                    </c:otherwise>
+						                </c:choose>
+						            </c:forEach>
+						        </tr>
+						    </table>
+						     
+						    <%--For displaying Next link --%>
+						    <c:if test="${currentPage lt noOfPages}">
+						        <td><a href="searchResults.do?page=${currentPage + 1}">Next</a></td>
+						    </c:if>
+									*/						
+						</table>
+																	
 					
 					
 										       
